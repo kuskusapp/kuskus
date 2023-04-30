91ed829f1fef744040dfd7516bf6fb692d081662
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-05-01 01:27:56 +0200

    ai

--- ai/src/main.rs
+++ ai/src/main.rs
@@ -1,16 +1,67 @@
 use axum::{
-    routing::get,
+    error_handling::HandleErrorLayer,
+    http::StatusCode,
+    routing::{patch},
     Router,
 };
+use std::{
+    net::SocketAddr,
+    time::Duration,
+};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_todos=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    // Compose the routes
+    let app = Router::new()
+        .route("/", get(todos_update))
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {}", error),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        )
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn get_ai_response() -> impl IntoResponse {
+    let todos = db.read().unwrap();
+
+    let Query(pagination) = pagination.unwrap_or_default();
+
+    let todos = todos
+        .values()
+        .skip(pagination.offset.unwrap_or(0))
+        .take(pagination.limit.unwrap_or(usize::MAX))
+        .cloned()
+        .collect::<Vec<_>>();
+
+    Json(todos)
 }
