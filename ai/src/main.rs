11370a724520686ee6cacab21114a2b73b4471c5
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-05-01 12:38:58 +0200

    route

--- ai/src/main.rs
+++ ai/src/main.rs
@@ -1,67 +1,30 @@
-use axum::{
-    error_handling::HandleErrorLayer,
-    http::StatusCode,
-    routing::{patch},
-    Router,
-};
-use std::{
-    net::SocketAddr,
-    time::Duration,
-};
-use tower::{BoxError, ServiceBuilder};
-use tower_http::trace::TraceLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use axum::{http::StatusCode, routing::get, Json, Router};
+use serde::Serialize;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "example_todos=debug,tower_http=debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
 
-    // Compose the routes
-    let app = Router::new()
-        .route("/", get(todos_update))
-        // Add middleware to all routes
-        .layer(
-            ServiceBuilder::new()
-                .layer(HandleErrorLayer::new(|error: BoxError| async move {
-                    if error.is::<tower::timeout::error::Elapsed>() {
-                        Ok(StatusCode::REQUEST_TIMEOUT)
-                    } else {
-                        Err((
-                            StatusCode::INTERNAL_SERVER_ERROR,
-                            format!("Unhandled internal error: {}", error),
-                        ))
-                    }
-                }))
-                .timeout(Duration::from_secs(10))
-                .layer(TraceLayer::new_for_http())
-                .into_inner(),
-        )
-
+    // run it, 3001 port is used as 3000 is web app for now
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
-    tracing::debug!("listening on {}", addr);
+    println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn get_ai_response() -> impl IntoResponse {
-    let todos = db.read().unwrap();
+async fn handler() -> (StatusCode, Json<MyJsonResponse>) {
+    let response = MyJsonResponse {
+        message: "Hello from JSON!".to_string(),
+    };
 
-    let Query(pagination) = pagination.unwrap_or_default();
+    (StatusCode::OK, Json(response))
+}
 
-    let todos = todos
-        .values()
-        .skip(pagination.offset.unwrap_or(0))
-        .take(pagination.limit.unwrap_or(usize::MAX))
-        .cloned()
-        .collect::<Vec<_>>();
-
-    Json(todos)
+#[derive(Serialize)]
+struct MyJsonResponse {
+    message: String,
 }
