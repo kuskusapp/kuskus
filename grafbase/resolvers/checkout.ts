c9e165dc8f5c3ef75e3dbe5aa7f778e13cbd9515
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-05-05 14:33:32 +0200

    stripe resolver

---
+++ grafbase/resolvers/checkout.ts
@@ -0,0 +1,18 @@
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2022-11-15",
+  typescript: true,
+})
+
+export default async function Resolver(_: any, { input }: any) {
+  const { lineItems } = input
+
+  const { data } = await stripe.checkout.sessions.create({
+    success_url: "https://example.com/success",
+    line_items: lineItems,
+    mode: "payment",
+  })
+
+  return { url: data.url }
+}
