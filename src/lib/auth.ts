b939ea9a34b99a5c3a06c740df3dfa651ef91003
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-05-05 14:01:47 +0200

    auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,7 +16,7 @@ export const GoogleClient = new UserManager({
   authority: "https://accounts.google.com",
   client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID as string,
   client_secret: import.meta.env.VITE_GOOGLE_SECRET as string,
-  redirect_uri: "http://localhost:3000/login/google",
+  redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT_URI as string,
   metadataSeed: {
     end_session_endpoint: "https://www.google.com/accounts/logout",
     issuer: "https://accounts.google.com",
