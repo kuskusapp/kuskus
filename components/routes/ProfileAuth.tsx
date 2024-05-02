ca888f8e557eb9fc0a34a9eac956d1d3adb72877
Nikita <nikita.voloboev@gmail.com>
2024-05-02 21:23:19 +0400

    legend state

--- components/routes/ProfileAuth.tsx
+++ components/routes/ProfileAuth.tsx
@@ -1,11 +1,18 @@
 "use client"
 
 import { profileAuthReturn } from "@/edgedb/crud/queries"
+import { observer, useObservable } from "@legendapp/state/react"
 
 interface Props {
   data: profileAuthReturn
 }
 
-export default function ProfileAuth(props: Props) {
-  return <></>
-}
+export default observer(function ProfileAuth(props: Props) {
+  const local$ = useObservable(props.data)
+
+  return (
+    <>
+      <div>Name: {local$.name.get()}</div>
+    </>
+  )
+})
