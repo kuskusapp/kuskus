8de4195b90d8eaa6ef515f01b2635f2e7aa6f4e8
marshennikovaolga <marshennikova@gmail.com>
2024-05-13 14:08:09 +0300

    .

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,7 +2,9 @@
 import { useState } from "react"
 import Image from "next/image"
 import { IoIosSearch } from "react-icons/io"
+import Link from "next/link"
 import {
+  GlobeIcon,
   GridIcon,
   SearchIcon,
   UserIcon,
@@ -19,37 +21,37 @@ const recommendedPlaces = [
     id: "place1",
     name: "Place 1",
     category: "coffeeshop",
-    imageUrl: "/https://a.jpg",
+    imageUrl: "https://images.omrshn.dev/logisticcars.jpeg",
   },
   {
     id: "place2",
     name: "Place 2",
     category: "coffeeshop",
-    imageUrl: "/https://a.jpg",
+    imageUrl: "https://images.omrshn.dev/logisticcars.jpeg",
   },
   {
     id: "place3",
     name: "Place 3",
     category: "coffeeshop",
-    imageUrl: "/https://a.jpg",
+    imageUrl: "https://images.omrshn.dev/logisticcars.jpeg",
   },
   {
     id: "place4",
     name: "Place 4",
     category: "coffeeshop",
-    imageUrl: "/https://a.jpg",
+    imageUrl: "https://images.omrshn.dev/logisticcars.jpeg",
   },
   {
     id: "place5",
     name: "Place 5",
     category: "coffeeshop",
-    imageUrl: "/https://a.jpg",
+    imageUrl: "https://images.omrshn.dev/logisticcars.jpeg",
   },
   {
     id: "place6",
     name: "Place 6",
     category: "coffeeshop",
-    imageUrl: "/https://a.jpg",
+    imageUrl: "https://images.omrshn.dev/logisticcars.jpeg",
   },
 ]
 
@@ -58,46 +60,75 @@ export default function Search() {
 
   return (
     <>
-      <div
-        style={{
-          position: "fixed",
-          top: 20,
-          right: 20,
-          width: "100%",
-          zIndex: 100,
-        }}
-        className="flex flex-row space-x-2 justify-center"
-      >
-        <div
-          className="mb-5 flex justify-center items-center space-x-3 rounded-full py-2"
-          style={{
-            width: "13%",
-            marginLeft: "auto",
-            marginRight: "10px",
-            justifyContent: "center",
-            backgroundColor: "rgb(47 47 48)",
-            boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.3)",
-          }}
-        >
-          <GridIcon className="text-white w-7 h-7" />
-          <SearchIcon className="text-white w-7 h-7" />
-          <UserIcon className="text-white w-7 h-7" />
-          <NotificationIcon className="text-white w-7 h-7" />
+      <div className="flex flex-row space-x-15">
+        <div className="flex flex-row space-x-4 mt-8 ml-10">
+          <Link href="/">
+            <p
+              className="text-base font-semibold text-neutral-500"
+              style={{ cursor: "pointer" }}
+            >
+              Places
+            </p>
+          </Link>
+          <Link href="#">
+            <p
+              className="text-base font-semibold text-neutral-500"
+              style={{ cursor: "pointer" }}
+            >
+              Members
+            </p>
+          </Link>
+          <Link href="#">
+            <p
+              className="text-base font-semibold text-neutral-500"
+              style={{ cursor: "pointer" }}
+            >
+              Dishes
+            </p>
+          </Link>
         </div>
 
-        <button
+        <div
           style={{
-            backgroundColor: "rgb(47 47 48)",
-            width: "40px",
-            height: "40px",
-            boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.3)",
+            position: "fixed",
+            top: 20,
+            right: 20,
+            width: "100%",
+            zIndex: 100,
           }}
-          className="rounded-full items-center justify-center flex text-white focus:outline-none focus:ring"
+          className="flex flex-row space-x-2 justify-center"
         >
-          <PlusIcon className="text-white w-7 h-7" />
-        </button>
+          <div
+            className="mb-5 flex justify-center items-center space-x-3 rounded-full py-2"
+            style={{
+              width: "13%",
+              marginLeft: "auto",
+              marginRight: "10px",
+              justifyContent: "center",
+              backgroundColor: "rgb(47 47 48)",
+              boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.3)",
+            }}
+          >
+            <GridIcon className="text-white w-7 h-7" />
+            <SearchIcon className="text-white w-7 h-7" />
+            <UserIcon className="text-white w-7 h-7" />
+            <NotificationIcon className="text-white w-7 h-7" />
+          </div>
+
+          <button
+            style={{
+              backgroundColor: "rgb(47 47 48)",
+              width: "40px",
+              height: "40px",
+              boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.3)",
+            }}
+            className="rounded-full items-center justify-center flex text-white focus:outline-none focus:ring"
+          >
+            <PlusIcon className="text-white w-7 h-7" />
+          </button>
+        </div>
       </div>
-      <div className="bg-white justify-center px-5 pt-5 mt-20">
+      <div className="bg-white justify-center px-5 pt-5 mt-5">
         <div
           className="relative flex items-center justify-center h-10 m-auto"
           style={{ width: "100%" }}
@@ -118,42 +149,18 @@ export default function Search() {
             className="absolute flex flex-row right-0 mr-1 px-4 py-2 rounded-full text-white bg-blue-400 hover:bg-blue-600 focus:outline-none focus:ring"
             style={{ top: "50%", transform: "translateY(-50%)" }}
           >
-            <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <circle
-                cx="12"
-                cy="12"
-                r="7.25"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-              />
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-                d="M15.25 12C15.25 16.5 13.2426 19.25 12 19.25C10.7574 19.25 8.75 16.5 8.75 12C8.75 7.5 10.7574 4.75 12 4.75C13.2426 4.75 15.25 7.5 15.25 12Z"
-              />
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-                d="M5 12H12H19"
-              />
-            </svg>
+            <GlobeIcon className="w-7 h-7 color-white" />
             or discover nearby dining
           </button>
         </div>
-        <div className="pb-20 space-y-20">
+        <div className="pb-20 space-y-10">
           <div className="pt-10">
             <h2 className="text-2xl font-normal">
               Places we recommend for you in{" "}
               <span className="">[City, Country]</span>
             </h2>
             <Swiper
-              className="mt-10"
+              className="mt-5"
               spaceBetween={20}
               slidesPerView={4}
               onSlideChange={() => console.log("slide change")}
@@ -182,7 +189,7 @@ export default function Search() {
           <div>
             <h2 className="text-2xl font-normal">Just Opened! Check it Out</h2>
             <Swiper
-              className="mt-10"
+              className="mt-5"
               spaceBetween={20}
               slidesPerView={4}
               onSlideChange={() => console.log("slide change")}
@@ -209,9 +216,9 @@ export default function Search() {
           </div>
 
           <div>
-            <h2 className="text-2xl font-normal">Text</h2>
+            <h2 className="text-2xl font-normal">Just Opened! Check it Out</h2>
             <Swiper
-              className="mt-10"
+              className="mt-5"
               spaceBetween={20}
               slidesPerView={4}
               onSlideChange={() => console.log("slide change")}
--- public/svg/search-icons.tsx
+++ public/svg/search-icons.tsx
@@ -1,3 +1,31 @@
+export const GlobeIcon = ({ className }: { className?: string }) => (
+  <svg className={className} fill="none" viewBox="0 0 24 24">
+    <circle
+      cx="12"
+      cy="12"
+      r="7.25"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M15.25 12C15.25 16.5 13.2426 19.25 12 19.25C10.7574 19.25 8.75 16.5 8.75 12C8.75 7.5 10.7574 4.75 12 4.75C13.2426 4.75 15.25 7.5 15.25 12Z"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M5 12H12H19"
+    />
+  </svg>
+)
+
 export const GridIcon = ({ className }: { className?: string }) => (
   <svg className={className} viewBox="0 0 24 24" fill="none">
     <path
