d3af3c6ea86006d56a0c5ae1e8bae506603e1cb9
Kisuyo <ig.intr3st@gmail.com>
2024-05-15 15:51:41 +0200

    .

--- components/routes/ProfileAuth.tsx
+++ components/routes/ProfileAuth.tsx
@@ -30,18 +30,19 @@ export default observer(function ProfileAuth(props: Props) {
     "image3",
     "image4",
     "image5",
+    "image6",
   ])
 
   return (
     <div className="min-h-screen h-full text-black/60">
       <Sidebar />
-      <div className="ml-[320px] h-full pl-[6px] grid grid-cols-2 gap-[6px]">
+      <div className="ml-[380px] h-full pl-[6px] grid grid-cols-2 gap-[6px]">
         {images.map((e, index) => {
           return (
             <div
               key={index}
               className="aspect-w-1 aspect-h-1 bg-gray-200"
-              style={{ paddingBottom: "100%" }}
+              style={{ paddingBottom: "70%" }}
             >
               {e}
             </div>
@@ -55,7 +56,7 @@ export default observer(function ProfileAuth(props: Props) {
 function Sidebar() {
   const [hoveredSidebarTab, setHoveredSidebarTab] = useState("Following")
   return (
-    <div className="fixed left-0 w-[320px] top-0 h-screen bg-gray-200">
+    <div className="fixed left-0 w-[380px] top-0 h-screen bg-gray-200">
       <div className="w-full h-3/5 bg-gray-300">Profile</div>
       <div className="p-[24px] pt-[34px] flex flex-col justify-between h-2/5">
         <div className="flex flex-col gap-[2px]">
