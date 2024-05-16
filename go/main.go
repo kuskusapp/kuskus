1f7e45c7c96f447f015f83ee2c1fb2506c64b4ad
Nikita <nikita.voloboev@gmail.com>
2024-05-16 11:51:21 +0200

    go

---
+++ go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/go-fuego/fuego"
+
+func main() {
+	s := fuego.NewServer()
+
+	fuego.Get(s, "/", func(c fuego.ContextNoBody) (string, error) {
+		return "Hello, World!", nil
+	})
+
+	s.Run()
+}
