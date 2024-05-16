e79b851871c8df21eadc833639bdd4fca7b598c6
Nikita <nikita.voloboev@gmail.com>
2024-05-16 13:51:58 +0200

    .

--- components/routes/ChatAuth.tsx
+++ components/routes/ChatAuth.tsx
@@ -1,10 +1,12 @@
 "use client"
-import { observer } from "@legendapp/state/react"
+import { observer, useObservable } from "@legendapp/state/react"
 
 interface Props {
 	data: any
 }
 
 export default observer(function ChatAuth(props: Props) {
+	const server = useObservable(props.data)
+	const local = useObservable({})
 	return <>chat here</>
 })
--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,8 +1,18 @@
 // using Grafbase currently to merge API endpoints in other languages into one GraphQL API
 // we then generate functions to interact with the API
-import { graph, config } from "@grafbase/sdk"
+import { graph, config, connector } from "@grafbase/sdk"
 
 const g = graph.Standalone()
+
+// https://gist.github.com/nikitavoloboev/39f667045e503fb170b245a14dc2209c
+// TODO: failing
+const goApi = connector.OpenAPI("Go", {
+	schema:
+		"https://gist.githubusercontent.com/nikitavoloboev/39f667045e503fb170b245a14dc2209c/raw/89fc98765327c158ca76f53934f13e3662a9cc93/kuskus-go-openapi.json",
+	url: "https://gist.githubusercontent.com/nikitavoloboev/39f667045e503fb170b245a14dc2209c/raw/89fc98765327c158ca76f53934f13e3662a9cc93/kuskus-go-openapi.json",
+})
+
+g.datasource(goApi)
 
 export default config({
 	graph: g,
