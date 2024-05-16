7c2573a34dcd625ff97d7b25ac88a192300473e2
Nikita <nikita.voloboev@gmail.com>
2024-05-16 23:15:02 +0200

    .

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 		<>
 			<Stack.Screen options={{ title: "Home" }} />
 			<Container>
-				<ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
+				<ScreenContent path="app/index.tsx" title="Home" />
 				<Link href={{ pathname: "/details", params: { name: "Dan" } }} asChild>
 					<Button title="Show Details" />
 				</Link>
