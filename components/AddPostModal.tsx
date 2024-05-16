26123ade8fcc0faa5a910b00c89a89a14d37ac2d
marshennikovaolga <marshennikova@gmail.com>
2024-05-16 19:55:50 +0300

    modal

--- components/AddPostModal.tsx
+++ components/AddPostModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Fragment } from "react"
 import { Dialog, Transition } from "@headlessui/react"
-import { PhotoIcon } from "../public/svg/modal-icons"
+import { PhotoIcon, AIcon } from "../public/svg/modal-icons"
 import { observer, useObservable } from "@legendapp/state/react"
 
 interface Props {
@@ -9,6 +9,19 @@ interface Props {
 	postsState: any
 }
 
+const foodCategories = [
+	"Pizza",
+	"Sushi",
+	"Burger",
+	"Pasta",
+	"Salad",
+	"Dessert",
+	"Steak",
+	"Tacos",
+	"Curry",
+	"Noodles",
+]
+
 export default observer(function AddPostModal(props: Props) {
 	const local = useObservable({
 		isOpen: props.open,
@@ -16,6 +29,26 @@ export default observer(function AddPostModal(props: Props) {
 		description: "",
 		image: null as File | null,
 	})
+
+	const [categories, setCategories] = useState<string[]>([])
+	const [initialCount, setInitialCount] = useState(6)
+
+	const addCathegory = (category: string) => {
+		setCategories((prevSelected) =>
+			prevSelected.includes(category)
+				? prevSelected.filter((cat) => cat !== category)
+				: [...prevSelected, category],
+		)
+	}
+
+	const viewMore = () => {
+		setInitialCount((prevCount) => prevCount + 4)
+	}
+
+	const sortedCategories = [
+		...categories,
+		...foodCategories.filter((cat) => !categories.includes(cat)),
+	].slice(0, initialCount)
 
 	const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.files && event.target.files[0]) {
@@ -50,7 +83,7 @@ export default observer(function AddPostModal(props: Props) {
 				>
 					x
 				</button>
-				<div className="min-h-screen px-4 text-center">
+				<div className="min-h-screen px-2 text-center">
 					<Transition.Child
 						as={Fragment}
 						enter="ease-out duration-300"
@@ -78,18 +111,18 @@ export default observer(function AddPostModal(props: Props) {
 						leaveFrom="opacity-100 scale-100"
 						leaveTo="opacity-0 scale-95"
 					>
-						<div className="inline-block w-full max-w-4xl p-8 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+						<div className="inline-block w-full max-w-4xl p-4 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
 							<form
 								onSubmit={(e) => {
 									e.preventDefault()
 									handleSubmit()
 								}}
 								className="flex gap-5"
-								style={{ minHeight: "500px" }}
+								style={{ minHeight: "600px" }}
 							>
 								<div
 									className="w-2/3 flex justify-center items-center m-auto"
-									style={{ borderRight: "1px solid #c5c5c5", height: "500px" }}
+									style={{ borderRight: "1px solid #c5c5c5", height: "590px" }}
 								>
 									<button
 										className="mt-1 w-full h-full flex justify-center items-center bg-white focus:outline-none"
@@ -104,41 +137,108 @@ export default observer(function AddPostModal(props: Props) {
 										/>
 									</button>
 								</div>
-								<div className="flex-1 flex flex-col">
-									<div className="flex-1">
-										<div className="mt-4">
-											<label
-												htmlFor="title"
-												className="block text-sm font-thin text-gray-700"
-											>
-												Title
-											</label>
-											<input
-												type="text"
-												id="title"
-												value={local.title.get()}
-												onChange={(e) => {
-													local.title.set(e.target.value)
+								<div className=" flex flex-col">
+									<div>
+										<label
+											htmlFor="description"
+											className="block text-sm font-thin text-gray-700 pb-2 mb-2"
+											style={{
+												borderBottom: "1px solid #c5c5c5",
+												width: "320px",
+											}}
+										>
+											Description
+										</label>
+										<textarea
+											id="description"
+											value={local.description.get()}
+											placeholder="Write a description..."
+											style={{
+												height: "150px",
+												outline: "none",
+												textAlign: "left",
+												resize: "none",
+												overflow: "auto",
+											}}
+											className="mt-1 block w-full px-3 bg-white border-none sm:text-sm textarea-placeholder"
+										/>
+									</div>
+									<div style={{ height: "150px" }}>
+										<label
+											className="block text-sm font-thin text-gray-700 pb-2 mb-2"
+											style={{
+												borderBottom: "1px solid #c5c5c5",
+												width: "320px",
+											}}
+										>
+											AI Description
+										</label>
+										<p className="font-thin text-sm pl-4">textext</p>
+										<div
+											style={{
+												position: "relative",
+												width: "320px",
+												height: "100px",
+											}}
+										>
+											<div
+												style={{
+													position: "absolute",
+													display: "flex",
+													flexDirection: "row",
+
+													gap: "2px",
+													right: "0",
+													bottom: "0",
 												}}
-												className="mt-1 block w-full px-3 py-2 rounded-md border-none shadow-sm focus:outline-none sm:text-sm"
-											/>
-										</div>
-										<div className="mt-4">
-											<label
-												htmlFor="description"
-												className="block text-sm font-thin text-gray-700"
 											>
-												Description
-											</label>
-											<input
-												id="description"
-												value={local.description.get()}
-												onChange={(e) => {
-													// setDescription(e.target.value)
-												}}
-												className="mt-1 block w-full px-3 py-2 bg-white border-none rounded-md shadow-sm focus:outline-none sm:text-sm"
-											/>
+												<AIcon className="spin text-purple-600 h-4 w-4" />
+												<p className="font-thin text-right text-xs">
+													AI is thinking
+												</p>
+											</div>
 										</div>
+									</div>
+									<div
+										style={{
+											width: "320px",
+											height: "100px",
+											position: "relative",
+										}}
+									>
+										<label
+											className="block text-sm font-thin text-gray-700 pb-2 mb-2"
+											style={{
+												borderBottom: "1px solid #c5c5c5",
+												width: "320px",
+											}}
+										>
+											Categories
+										</label>
+										<input
+											placeholder="search categories..."
+											className="mt-1 block w-full px-3 bg-white border-none sm:text-sm textarea-placeholder"
+										></input>
+
+										<div className="flex flex-wrap gap-2 mt-2">
+											{sortedCategories.map((category) => (
+												<button
+													key={category}
+													className={`px-3 py-1 text-gray-500 font-normal border rounded-full ${categories.includes(category) ? "bg-yellow-500 text-white" : "hover:border-yellow-500"}`}
+													onClick={() => addCathegory(category)}
+												>
+													{category}
+												</button>
+											))}
+										</div>
+										{initialCount < foodCategories.length && (
+											<button
+												className="mt-2 text-gray-500 font-thin cursor-pointer"
+												onClick={viewMore}
+											>
+												view more
+											</button>
+										)}
 									</div>
 								</div>
 							</form>
--- public/svg/modal-icons.tsx
+++ public/svg/modal-icons.tsx
@@ -109,3 +109,15 @@ export const PhotoIcon = ({ className }: { className?: string }) => (
 		></path>
 	</svg>
 )
+
+export const AIcon = ({ className }: { className?: string }) => (
+	<svg className={className} viewBox="0 0 24 24" fill="none">
+		<path
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="M15 5.75C15 7.5 13.5 9 11.75 9 13.5 9 15 10.5 15 12.25 15 10.5 16.5 9 18.25 9 16.5 9 15 7.5 15 5.75Zm-7 8C8 16 8 16 5.75 16 8 16 8 16 8 18.25 8 16 8 16 10.25 16 8 16 8 16 8 13.75Zm9 1L14.75 17 17 19.25 19.25 17 17 14.75Zm-10-9L5.75 7 7 8.25 8.25 7 7 5.75Z"
+		></path>
+	</svg>
+)
