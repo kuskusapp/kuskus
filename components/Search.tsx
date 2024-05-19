a99ee8eb8b96e25aca1fd52bd9b750563996508b
Kisuyo <ig.intr3st@gmail.com>
2024-05-19 13:43:47 +0200

    search

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,23 +3,25 @@ import Icons from "./Icons"
 
 export default function Search() {
 	return (
-		<div className="flex-between border-neutral-500 border bg-white rounded-md w-[620px] p-1">
-			<div className="flex items-center flex-grow">
-				<IoIosSearch className="ml-3 text-black" size={20} />
-				<input
-					className="border flex-grow  bg-transparent border-none text-black outline-none px-4 p-2"
-					placeholder="Search for a place or dish..."
-					onFocus={() => {
-						// setInputFocused(true)
-					}}
-					onBlur={() => {
-						// setInputFocused(false)
-					}}
-				/>
+		<div className="flex gap-[10px] w-full">
+			<div className="flex-between border-white/10 border bg-secondary rounded-[11px] w-full p-1">
+				<div className="flex items-center flex-grow">
+					<IoIosSearch className="ml-3 " size={20} />
+					<input
+						className="border flex-grow  bg-transparent border-none outline-none px-4 p-2"
+						placeholder="Search for a place or dish..."
+						onFocus={() => {
+							// setInputFocused(true)
+						}}
+						onBlur={() => {
+							// setInputFocused(false)
+						}}
+					/>
+				</div>
 			</div>
-			<button className="flex gap-[4px] right-0 px-4 py-2 text-[14px] rounded-md text-white flex-center bg-secondary focus:outline-none focus:ring">
+			<button className="flex w-[180px] gap-[4px] right-0 px-4 py-2 rounded-[11px] text-black flex-center bg-orange-200 border-b-2 border-yellow-600 focus:outline-none focus:ring">
 				<Icons name="World" />
-				or find places near you
+				Find nearby
 			</button>
 		</div>
 	)
