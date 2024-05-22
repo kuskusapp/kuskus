d9f00197fd532f7d57d3a7b2c97a736ecf7138d9
marshennikovaolga <marshennikova@gmail.com>
2024-05-22 18:59:05 +0300

    .

--- components/ViewPost.tsx
+++ components/ViewPost.tsx
@@ -20,7 +20,6 @@ interface Props {
 export default function ViewPost(props: Props) {
 	return (
 		<div className="fixed bg-neutral-900 bg-opacity-95 w-screen z-[100] h-screen flex [&::-webkit-scrollbar]:hidden backdrop-blur-sm">
-			{/* <div className="w-24 h-20 p-5 flex justify-center"> */}
 			<button
 				className="absolute top-10 left-20 glass-background hover:opacity-40 px-3 py-3 rounded-full z-50"
 				onClick={() => {
@@ -29,8 +28,7 @@ export default function ViewPost(props: Props) {
 			>
 				<IoCloseOutline size={20} />
 			</button>
-			{/* </div> */}
-			<div className="w-[80%] bg-black h-[80%] flex absolute top-[10%] left-[10%] rounded-lg shadow-xl">
+			<div className="w-[85%] bg-black h-[80%] flex absolute top-[10%] left-[10%] rounded-lg shadow-xl">
 				<div className="w-3/5 z-20 overflow-hidden relative flex-center h-full">
 					<div
 						style={{
@@ -60,24 +58,19 @@ export default function ViewPost(props: Props) {
 							molly
 						</div>
 						<div className="flex flex-col text-[14px] text-white/70">
-							<div className="text-primaryText flex">
-								<div className="w-[38px]"></div>
+							<p className="text-primaryText flex">
 								Lorem ipsum dolor sit amet consectetur adipisicing elit.
 								Numquam, repudiandae impedit. Quisquam ipsa sunt, ducimus maxime
 								dolores consequuntur. Ad, quaerat? Esse aut facere blanditiis
 								quod veritatis corporis accusamus eos unde.
-							</div>
-							<div className="text-primaryText flex">
-								<div className="w-[38px]"></div>
-								japanese
-							</div>
-							<div className="flex items-center">
-								<div className="w-[38px]">
+							</p>
+							<div className="flex-col pt-2 space-y-1">
+								<p>japanese</p>
+								<div className="flex-row">
 									<Icons name="Tag" size={[22, 22]} />
+									<p>tags</p>
 								</div>
-								tags
 							</div>
-							<div></div>
 						</div>
 					</div>
 					<div className="p-[20px] border-y border-white/10 h-full">
