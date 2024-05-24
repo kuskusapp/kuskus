043d26671df446386f672692949c872ec5dbd005
marshennikovaolga <marshennikova@gmail.com>
2024-05-24 21:05:04 +0300

    ai chat

--- components/routes/Chat.tsx
+++ components/routes/Chat.tsx
@@ -6,61 +6,86 @@ import { useEffect } from "react"
 import PlaceCard from "../PlaceCard"
 import ActionBar from "../ActionBar"
 
+interface RelevantPlace {
+	name: string
+	displayName: string
+	imageUrl: string
+	category: string
+}
+
 export default observer(function Chat() {
 	const local = useObservable({
 		query: "",
 		messages: [] as CoreMessage[],
-		relevantPlaces: [] as {
-			name: string
-			displayName: string
-			imageUrl: string
-			category: string
+		questionAndAnswer: [] as {
+			question: string
+			relevantPlaces?: RelevantPlace[]
+			answerAsText?: string
 		}[],
-		firstQuestionAsked: false,
 	})
 
+	// used for testing the ui
 	useEffect(() => {
-		local.relevantPlaces.set([
+		local.questionAndAnswer.set([
 			{
-				name: "sklep-z-kawa-i-kawiarnia",
-				displayName: "Sklep z Kawą i Kawiarnia",
-				imageUrl:
-					"https://lh5.googleusercontent.com/p/AF1QipPB8FL_x-CQbk9z4ZYLkaqyrHNfkhnFhJ5-T0Qw=w408-h271-k-no",
-				category: "coffee",
-			},
-			{
-				name: "wazaap",
-				displayName: "Wazaap",
-				imageUrl:
-					"https://lh5.googleusercontent.com/p/AF1QipPBtbz87EZ9HGIia9bkRT2szr99d5Bg9PRvLKvc=w408-h306-k-no",
-				category: "coffee",
+				question: "Great coffee places in Warsaw",
+				relevantPlaces: [
+					{
+						name: "sklep-z-kawa-i-kawiarnia",
+						displayName: "Sklep z Kawą i Kawiarnia",
+						imageUrl:
+							"https://lh5.googleusercontent.com/p/AF1QipPB8FL_x-CQbk9z4ZYLkaqyrHNfkhnFhJ5-T0Qw=w408-h271-k-no",
+						category: "coffee",
+					},
+					{
+						name: "wazaap",
+						displayName: "Wazaap",
+						imageUrl:
+							"https://lh5.googleusercontent.com/p/AF1QipPBtbz87EZ9HGIia9bkRT2szr99d5Bg9PRvLKvc=w408-h306-k-no",
+						category: "coffee",
+					},
+				],
 			},
 		])
 	}, [])
 
 	return (
 		<>
-			{/* {local.firstQuestionAsked.get() && <></>} */}
 			<div className="h-screen w-screen flex">
 				<div className="h-full bg-primary w-full flex flex-col px-[100px] p-2 pt-[50px]">
-					<div className="h-[90%] w-full"></div>
-					{local.relevantPlaces.get().map((place) => {
+					{local.questionAndAnswer.get().map((qa) => {
 						return (
-							<div
-								key={place.name}
-								className="flex-col w-[40%] items-center absolute left-1/2 top-10 transform -translate-x-1/2 bg-[#09090b] py-8 border border-zinc-700 rounded-md"
-							>
-								<p className="text-lg">Welcome to KusKus AI Chat!</p>
-								<p className="text-md w-[80%] text-left pt-5">
-									KusKus is your smart companion for discovering the best spots
-									nearby. Just type in the chat, and KusKus will swiftly suggest
-									restaurants, cafes, shops, and more around you. It's all about
-									making your search easy, fast, and perfectly tailored to your
-									needs!
-								</p>
-							</div>
+							<>
+								<div>question: {qa.question}</div>
+								<div>
+									answer:{" "}
+									{qa.relevantPlaces.map((place) => {
+										console.log(place.imageUrl)
+										return (
+											<PlaceCard
+												name={place.name}
+												displayName={place.displayName}
+												imageUrl={place.imageUrl}
+												category={place.category}
+											/>
+										)
+									})}
+								</div>
+							</>
 						)
 					})}
+					{local.questionAndAnswer.get().length === 0 && (
+						<div className="flex-col w-[40%] pl-10 absolute left-1/2 top-10 transform -translate-x-1/2 bg-[#09090b] py-8 border border-zinc-700 rounded-md">
+							<p className="text-lg text-left">Welcome to KusKus AI Chat!</p>
+							<p className="text-md w-[80%] text-left pt-5">
+								KusKus is your smart companion for discovering the best spots
+								nearby. Just type in the chat, and KusKus will swiftly suggest
+								restaurants, cafes, shops, and more around you. It's all about
+								making your search easy, fast, and perfectly tailored to your
+								needs!
+							</p>
+						</div>
+					)}
 					<div className="h-[10%] p-2 flex items-center justify-center">
 						<form
 							onSubmit={async (e) => {
