56dabdbeb5e8475a506cab51d003114d328748cc
Nikita <nikita.voloboev@gmail.com>
2024-05-26 01:34:46 +0200

    test server action

--- components/routes/SettingsRoute.tsx
+++ components/routes/SettingsRoute.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { updateUserProfileAction } from "@/app/actions"
+import { incrementNumberAction } from "@/app/new-actions"
 import Loader from "@/components/Loader"
 import { settingsAuthReturn } from "@/edgedb/crud/queries"
 import { observer, useObservable } from "@legendapp/state/react"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
+import { useServerAction } from "zsa-react"
 
 // TODO: add more settings, logout, ..
 interface Props {
@@ -21,6 +23,7 @@ export default observer(function SettingsRoute(props: Props) {
 		uploadedProfileImageAsFile: null as File | null,
 	})
 	const router = useRouter()
+	const { isPending, execute, data } = useServerAction(incrementNumberAction)
 
 	const handleBlur = () => {
 		local.touched.set(true)
@@ -54,6 +57,10 @@ export default observer(function SettingsRoute(props: Props) {
 							disabled={!local.username.get()}
 							className={`bg-black rounded-full h-[34px] flex items-center px-4 py-2 font-semibold text-[12px] text-white ${local.username.get() ? "hover:text-gray-200 hover:bg-neutral-800" : "bg-gray-500 cursor-not-allowed"}`}
 							onClick={async () => {
+								const [data, err] = await execute({ number: 5 })
+								console.log(data)
+
+								return
 								local.savingProfile.set(true)
 								const formData = new FormData()
 								formData.append(
