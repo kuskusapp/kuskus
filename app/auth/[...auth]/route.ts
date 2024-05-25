1d4d231460bda585d83e3e9d22504a244784002a
Nikita <nikita.voloboev@gmail.com>
2024-05-25 17:27:28 +0200

    auth

--- app/auth/[...auth]/route.ts
+++ app/auth/[...auth]/route.ts
@@ -16,32 +16,52 @@ export const { GET, POST } = auth.createAuthRouteHandlers({
 			console.log("email verification required")
 		}
 		if (isSignUp) {
-			const octokit = new Octokit({ auth: tokenData?.provider_token })
-			const result = await octokit.request("GET /user", {
-				headers: {
-					"X-GitHub-Api-Version": "2022-11-28",
-				},
-			})
-			const client = auth.getSession().client
+			if (process.env.NODE_ENV === "production") {
+				const octokit = new Octokit({ auth: tokenData?.provider_token })
+				const result = await octokit.request("GET /user", {
+					headers: {
+						"X-GitHub-Api-Version": "2022-11-28",
+					},
+				})
+				const client = auth.getSession().client
 
-			await client.query(
-				`
-        INSERT User {
-          name := <optional str>$name,
-          email := <optional str>$email,
-          githubUsername := <optional str>$githubUsername,
-          githubAvatarUrl := <optional str>$avatarUrl,
-          userRole := 'user',
-          identity := (global ext::auth::ClientTokenIdentity)
-        }
-      `,
-				{
-					name: result?.data?.name,
-					email: result?.data?.name,
-					githubUsername: result?.data?.login,
-					githubAvatarUrl: result?.data?.avatar_url,
-				},
-			)
+				await client.query(
+					`
+					INSERT User {
+						name := <optional str>$name,
+						email := <optional str>$email,
+						githubUsername := <optional str>$githubUsername,
+						githubAvatarUrl := <optional str>$avatarUrl,
+						userRole := 'user',
+						identity := (global ext::auth::ClientTokenIdentity)
+					}
+				`,
+					{
+						name: result?.data?.name,
+						email: result?.data?.name,
+						githubUsername: result?.data?.login,
+						githubAvatarUrl: result?.data?.avatar_url,
+					},
+				)
+			} else {
+				// TODO: only here because GitHub auth breaks down locally for some reason
+				const client = auth.getSession().client
+
+				const emailData = await client.querySingle<{ email: string }>(`
+					SELECT ext::auth::EmailFactor {
+						email
+					} FILTER .identity = (global ext::auth::ClientTokenIdentity)
+				`)
+
+				await client.query(`
+					INSERT User {
+						name := '',
+						email := '${emailData?.email}',
+						userRole := 'user',
+						identity := (global ext::auth::ClientTokenIdentity)
+					}
+				`)
+			}
 		}
 		redirect("/")
 	},
