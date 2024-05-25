20e7dee90affc522d27911d12c6cfc76dfa7a043
Nikita <nikita.voloboev@gmail.com>
2024-05-26 01:13:28 +0200

    fix prod

--- app/place/[name]/page.tsx
+++ app/place/[name]/page.tsx
@@ -1,17 +1,11 @@
 import { auth } from "@/edgedb-next-client"
-import { placesAuth } from "@/edgedb/crud/queries"
 import type { PageProps } from "./$types"
-import Place from "@/components/routes/Place"
 
-export default async function PlaceRoute({ params }: PageProps) {
+// TODO: non used route for now
+export default async function Place({ params }: PageProps) {
 	const session = auth.getSession()
 	const client = session.client
 	const authenticated = await session.isSignedIn()
-
-	if (!authenticated) {
-		return <></>
-	}
-
-	const data = await placesAuth.run(client, { placeName: params.name })
-	return <Place data={data} />
+	// const data = await placesAuth.run(client, { placeName: params.name })
+	return <></>
 }
