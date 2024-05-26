704e24330878c27814d19b9c419502cd42efb121
Nikita <nikita.voloboev@gmail.com>
2024-05-26 02:06:57 +0200

    server action test

--- app/actions.ts
+++ app/actions.ts
@@ -5,6 +5,7 @@ import z from "zod"
 import OpenAI from "openai"
 import { auth } from "@/edgedb-next-client"
 import { updateUser } from "@/edgedb/crud/mutations"
+import { create } from "ronin"
 
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
@@ -21,21 +22,40 @@ export const incrementNumberAction = createServerAction()
 		return input.number + 1
 	})
 
-export const updateUserAction = createServerAction()
+export const updateUserProfileAction = createServerAction()
 	.input(
 		z.object({
-			bio: z.string().optional(),
-			place: z.string().optional(),
+			username: z.string(),
 			displayName: z.string().optional(),
+			profileImage: z.custom<File>((file) => file instanceof File),
 		}),
+		{ type: "formData" },
 	)
 	.handler(async ({ input }) => {
-		const { bio, place, displayName } = input
+		const { username, displayName, profileImage } = input
 		const session = auth.getSession()
 		const client = session.client
-		try {
-			await updateUser.run(client, { bio, place, displayName })
-		} catch (err) {
-			return { failure: "EdgeDB error:", errorDetails: err.message }
+		if (profileImage) {
+			const resRoninUpload = await create.user.with({
+				profilePhotoUrl: profileImage,
+			})
+			const resUpdateUser = await updateUser.run(client, {
+				username,
+				displayName,
+				roninId: resRoninUpload.id,
+				profilePhotoUrl: resRoninUpload.profilePhotoUrl.src,
+			})
+			if (resUpdateUser) {
+				return "ok"
+			}
+		} else {
+			const resUpdateUser = await updateUser.run(client, {
+				username,
+				displayName,
+			})
+			if (resUpdateUser) {
+				return "ok"
+			}
 		}
+		throw new Error("Error updating user profile")
 	})
--- components/routes/SettingsRoute.tsx
+++ components/routes/SettingsRoute.tsx
@@ -1,12 +1,12 @@
 "use client"
-import { updateUserAction } from "@/app/actions"
+import { updateUserProfileAction } from "@/app/actions"
 import Loader from "@/components/Loader"
 import { settingsAuthReturn } from "@/edgedb/crud/queries"
+import { errorToast } from "@/src/utils"
 import { observer, useObservable } from "@legendapp/state/react"
-import { exec } from "child_process"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
-import { useServerAction } from "zsa-react"
+import { Toaster } from "react-hot-toast"
 
 // TODO: add more settings, logout, ..
 interface Props {
@@ -33,146 +33,151 @@ export default observer(function SettingsRoute(props: Props) {
 	}, [])
 
 	return (
-		<div className="w-screen flex z-30 h-screen bg-primary rounded-[20px] overflow-hidden">
-			<div className="border-r border-black/20 h-full w-1/2">
-				<div className="h-[80px] p-5 pb-4 text-[20px] font-semibold flex items-end border-b border-black/20">
-					Settings
-				</div>
-				<div className="flex flex-col overflow-auto max-h-[calc(100%-80px)] [&::-webkit-scrollbar]:hidden">
-					<div
-						className={`p-2 px-5 border-b border-white/10 flex justify-between items-center bg-white/10`}
-					>
-						Edit Profile
+		<>
+			<Toaster />
+			<div className="w-screen flex z-30 h-screen bg-primary rounded-[20px] overflow-hidden">
+				<div className="border-r border-black/20 h-full w-1/2">
+					<div className="h-[80px] p-5 pb-4 text-[20px] font-semibold flex items-end border-b border-black/20">
+						Settings
 					</div>
-				</div>
-				<div className="flex flex-col overflow-auto max-h-[calc(100%-80px)] [&::-webkit-scrollbar]:hidden"></div>
-			</div>
-			<div className="h-full w-1/2">
-				<div className="h-[80px] p-5 pb-4 justify-between flex items-end border-b border-black/20">
-					<div className="text-[20px] font-semibold"></div>
-					{local.savingProfile.get() && <Loader />}
-					{!local.savingProfile.get() && (
-						<button
-							disabled={!local.username.get()}
-							className={`bg-black rounded-full h-[34px] flex items-center px-4 py-2 font-semibold text-[12px] text-white ${local.username.get() ? "hover:text-gray-200 hover:bg-neutral-800" : "bg-gray-500 cursor-not-allowed"}`}
-							onClick={async () => {
-								// const [data, err] = await execute({ number: 5 })
-								// console.log(data)
-								// return
-								local.savingProfile.set(true)
-								const formData = new FormData()
-								formData.append(
-									"profileImage",
-									// @ts-ignore
-									local.uploadedProfileImageAsFile.get(),
-								)
-
-								// const res = await updateUserAction()
-								// const resUpdateUserProfileAction =
-								// 	await updateUserProfileAction({
-								// 		username: local.username.get(),
-								// 		displayName: local.displayName.get(),
-								// 		profileImage: formData,
-								// 	})
-								// if (!resUpdateUserProfileAction.serverError) {
-								// 	router.push("/")
-								// } else {
-								// 	// TODO: show in toast
-								// 	console.log(
-								// 		resUpdateUserProfileAction.serverError,
-								// 		"server error",
-								// 	)
-								// }
-								local.savingProfile.set(false)
-							}}
+					<div className="flex flex-col overflow-auto max-h-[calc(100%-80px)] [&::-webkit-scrollbar]:hidden">
+						<div
+							className={`p-2 px-5 border-b border-white/10 flex justify-between items-center bg-white/10`}
 						>
-							Save
-						</button>
-					)}
-				</div>
-
-				<div className="overflow-auto max-h-[calc(100%-80px)] [&::-webkit-scrollbar]:hidden cursor-pointer">
-					<div className="flex flex-col gap-[12px]">
-						<div className="w-full">
-							<div className="w-full bg-neutral-800 h-[400px] relative">
-								{local.uploadedProfileImageAsFile.get() && (
-									<img
-										src={URL.createObjectURL(
-											// @ts-ignore
-											local.uploadedProfileImageAsFile.get(),
-										)}
-										className="max-w-full max-h-full"
-									/>
-								)}
-								{local.profilePhotoUrl.get() && (
-									<img
-										src={local.profilePhotoUrl.get()}
-										className="max-w-full max-h-full"
-									/>
-								)}
-								<input
-									type="file"
-									id="profilePhotoUpload"
-									style={{ display: "none" }}
-									onChange={async (e: React.ChangeEvent<HTMLInputElement>) => {
-										if (e.target.files && e.target.files[0]) {
-											try {
-												const uploadedFile = e.target.files[0]
-												local.uploadedProfileImageAsFile.set(uploadedFile)
-												local.profilePhotoUrl.set("")
-											} catch (err) {
-												console.log(err)
-											}
-										}
-									}}
-								/>
-								<button
-									onClick={() =>
-										document.getElementById("profilePhotoUpload").click()
-									}
-									className="absolute bottom-2 left-2 rounded-full w-[70px] h-[70px] bg-neutral-700 bg-opacity-70 hover:bg-opacity-90 flex items-center justify-center"
-								>
-									{local.profilePhotoUrl.get() ? "Change" : "Add"}
-								</button>
-							</div>
-							<div className="flex gap-1">
-								{!local.username.get() && (
-									<div className="text-red-500 text-[12px] p-1">
-										Username Required
-									</div>
-								)}
-								{local.username.get() && (
-									<div className="text-[12px] p-1">Username</div>
-								)}
-							</div>
-							<input
-								type="text"
-								placeholder="Your username..."
-								className={`font-thin text-xs outline-none bg-transparent border-x-0 border-white/20 w-full ${local.touched.get() && !local.username.get() ? "border-red-500" : ""}`}
-								value={local.username.get()}
-								onChange={(e) => {
-									local.username.set(e.target.value)
-								}}
-								onBlur={handleBlur}
-							/>
+							Edit Profile
 						</div>
-						<div className="w-full">
-							<div className="font-light mb-[6px] text-white/50 px-4 text-[12px]">
-								Display Name
-							</div>
-							<input
-								type="text"
-								value={local.displayName.get()}
-								placeholder="Your name..."
-								onChange={(e) => {
-									local.displayName.set(e.target.value)
+					</div>
+					<div className="flex flex-col overflow-auto max-h-[calc(100%-80px)] [&::-webkit-scrollbar]:hidden"></div>
+				</div>
+				<div className="h-full w-1/2">
+					<div className="h-[80px] p-5 pb-4 justify-between flex items-end border-b border-black/20">
+						<div className="text-[20px] font-semibold"></div>
+						{local.savingProfile.get() && <Loader />}
+						{!local.savingProfile.get() && (
+							<button
+								disabled={!local.username.get()}
+								className={`bg-black rounded-full h-[34px] flex items-center px-4 py-2 font-semibold text-[12px] text-white ${local.username.get() ? "hover:text-gray-200 hover:bg-neutral-800" : "bg-gray-500 cursor-not-allowed"}`}
+								onClick={async () => {
+									// const [data, err] = await execute({ number: 5 })
+									// console.log(data)
+									// return
+									local.savingProfile.set(true)
+									const formData = new FormData()
+									formData.append(
+										"profileImage",
+										// @ts-ignore
+										local.uploadedProfileImageAsFile.get(),
+									)
+									const [data, err] = await updateUserProfileAction(formData, {
+										username: local.username.get(),
+										displayName: local.displayName.get(),
+									})
+
+									// const resUpdateUserProfileAction =
+									// 	await updateUserProfileAction({
+									// 		username: local.username.get(),
+									// 		displayName: local.displayName.get(),
+									// 		profileImage: formData,
+									// 	})
+
+									if (data) {
+										router.push("/")
+									} else {
+										errorToast(JSON.stringify(err))
+									}
+									local.savingProfile.set(false)
 								}}
-								className="font-thin text-xs outline-none bg-transparent border-x-0 border-white/20 w-full"
-							/>
+							>
+								Save
+							</button>
+						)}
+					</div>
+
+					<div className="overflow-auto max-h-[calc(100%-80px)] [&::-webkit-scrollbar]:hidden cursor-pointer">
+						<div className="flex flex-col gap-[12px]">
+							<div className="w-full">
+								<div className="w-full bg-neutral-800 h-[400px] relative">
+									{local.uploadedProfileImageAsFile.get() && (
+										<img
+											src={URL.createObjectURL(
+												// @ts-ignore
+												local.uploadedProfileImageAsFile.get(),
+											)}
+											className="max-w-full max-h-full"
+										/>
+									)}
+									{local.profilePhotoUrl.get() && (
+										<img
+											src={local.profilePhotoUrl.get()}
+											className="max-w-full max-h-full"
+										/>
+									)}
+									<input
+										type="file"
+										id="profilePhotoUpload"
+										style={{ display: "none" }}
+										onChange={async (
+											e: React.ChangeEvent<HTMLInputElement>,
+										) => {
+											if (e.target.files && e.target.files[0]) {
+												try {
+													const uploadedFile = e.target.files[0]
+													local.uploadedProfileImageAsFile.set(uploadedFile)
+													local.profilePhotoUrl.set("")
+												} catch (err) {
+													console.log(err)
+												}
+											}
+										}}
+									/>
+									<button
+										onClick={() =>
+											document.getElementById("profilePhotoUpload").click()
+										}
+										className="absolute bottom-2 left-2 rounded-full w-[70px] h-[70px] bg-neutral-700 bg-opacity-70 hover:bg-opacity-90 flex items-center justify-center"
+									>
+										{local.profilePhotoUrl.get() ? "Change" : "Add"}
+									</button>
+								</div>
+								<div className="flex gap-1">
+									{!local.username.get() && (
+										<div className="text-red-500 text-[12px] p-1">
+											Username Required
+										</div>
+									)}
+									{local.username.get() && (
+										<div className="text-[12px] p-1">Username</div>
+									)}
+								</div>
+								<input
+									type="text"
+									placeholder="Your username..."
+									className={`font-thin text-xs outline-none bg-transparent border-x-0 border-white/20 w-full ${local.touched.get() && !local.username.get() ? "border-red-500" : ""}`}
+									value={local.username.get()}
+									onChange={(e) => {
+										local.username.set(e.target.value)
+									}}
+									onBlur={handleBlur}
+								/>
+							</div>
+							<div className="w-full">
+								<div className="font-light mb-[6px] text-white/50 px-4 text-[12px]">
+									Display Name
+								</div>
+								<input
+									type="text"
+									value={local.displayName.get()}
+									placeholder="Your name..."
+									onChange={(e) => {
+										local.displayName.set(e.target.value)
+									}}
+									className="font-thin text-xs outline-none bg-transparent border-x-0 border-white/20 w-full"
+								/>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 })
